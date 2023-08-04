48e6ad98c823b9367b870f7ec7aedf536e10d9e5
Leon Farchau <leon2225@users.noreply.github.com>
2023-08-04 20:09:51 +0200

    Implement Point

---
+++ firmware/point.h
@@ -0,0 +1,35 @@
+/**
+ * @file point.h
+ * @author Leon Farchau (leon2225)
+ * @brief Class that represents a point
+ * @version 0.1
+ * @date 04.08.2023
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#pragma once
+
+#include "stdint.h"
+
+class Point
+{
+    public:
+        Point(){Point(0,0);}
+        Point( uint16_t x, uint16_t y ){ this->x = x; this->y = y;}
+        ~Point(){};
+
+        uint16_t x;
+        uint16_t y;
+
+        bool operator==(const Point& other) const {
+            return (this->x == other.x) && (this->y == other.y);}
+        bool operator!=(const Point& other) const {
+            return !(*this == other);}
+        Point operator+(const Point& other) const {
+            return Point(this->x + other.x, this->y + other.y);}
+        Point operator-(const Point& other) const {
+            return Point(this->x - other.x, this->y - other.y);}
+
+};
