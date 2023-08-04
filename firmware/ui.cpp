aacec2c10669e7cbdd03530a3d3669f8adf94216
Leon Farchau <leon2225@users.noreply.github.com>
2023-08-04 20:13:59 +0200

    Update UI to new changes

--- firmware/ui.cpp
+++ firmware/ui.cpp
@@ -18,6 +18,7 @@
 #include "pico/stdlib.h"
 #include "math.h"
 #include <string>
+#include <vector>
 
 #include "ili9488.h"
 #include "xpt2046.h"
@@ -25,6 +26,7 @@
 #include "rectangle.h"
 #include "text.h"
 #include "label.h"
+#include "button.h"
 
 ////////////////////////////////////////
 // Defines
@@ -57,6 +59,9 @@ void ui_updateTouch();
 ////////////////////////////////////////
 uint32_t g_nextTime_display = 0;
 uint32_t g_nextTime_touch = 0;
+
+std::vector<Button*> g_buttons;
+
 
 ////////////////////////////////////////
 // Functions
@@ -94,46 +99,48 @@ void ui_loop() {
  * 
  */
 void ui_updateDisplay() {
-    const uint32_t crossSize = 20;
+    static Point pointerPos = Point(15, 15);
+    static bool pressed = false;
 
-    static uint16_t x = 15, y = 15;
-
-    static Rectangle verticalLine = Rectangle(x-1, y-10, 2, crossSize, ILI9488_COLOR_RED);
-    static Rectangle horizontalLine = Rectangle(x-10, y-1, crossSize, 2, ILI9488_COLOR_RED);
-    static Text text = Text(0, 0, "", ILI9488_COLOR_WHITE, ILI9488_COLOR_BLACK, eILI9488_FONT_24);
-    static Label label = Label(100, 100, 250, 50, "Test", ILI9488_COLOR_YELLOW, ILI9488_COLOR_BLACK, eILI9488_FONT_24, LabelAlignment::CENTER);
+    static Label label = Label(Point(100, 100), Point(250, 50), "Test", ILI9488_COLOR_YELLOW, ILI9488_COLOR_BLACK, eILI9488_FONT_24, LabelAlignment::CENTER);
 
     const int redrawDistance = 2;
 
     XPT2046_TouchData_t touch = XPT2046_getTouch();
 
     if (touch.pressure) {
-        if((abs(touch.x - x) > redrawDistance) || (abs(touch.y - x) > redrawDistance))
+        if(!pressed)
         {
-            x = touch.x;
-            y = touch.y;
+            pressed = true;
+            label.setBgColor(ILI9488_COLOR_RED);
+        }
+
+        Point diff = touch.position - pointerPos;
+
+        if((abs(diff.x) > redrawDistance) || (abs(diff.y) > redrawDistance))
+        {
+            pointerPos = touch.position;
 
             gpio_put(DEBUG1_PIN, 1);
-            verticalLine.erase(ILI9488_COLOR_WHITE);
-            horizontalLine.erase(ILI9488_COLOR_WHITE);
 
-            verticalLine.setX(x-1);
-            verticalLine.setY(y -crossSize/2);
-            horizontalLine.setX(x -crossSize/2);
-            horizontalLine.setY(y-1);
-            std::string str = "X:" + std::to_string(touch.x) + ", Y:" + std::to_string(touch.y);
-            text.setText(str);
+            std::string str = "X:" + std::to_string(pointerPos.x) + ", Y:" + std::to_string(pointerPos.y);
             label.setText(str);
             
 
-            //text.draw();
-            verticalLine.draw();
-            horizontalLine.draw();
             label.draw();
 
             gpio_put(DEBUG1_PIN, 0);
         } 
     }
+    else
+    {
+        if(pressed)
+        {
+            pressed = false;
+            label.setBgColor(ILI9488_COLOR_YELLOW);
+            label.draw();
+        }
+    }   
 }
 
 /**
@@ -141,7 +148,21 @@ void ui_updateDisplay() {
  * 
  */
 void ui_updateTouch() {
+    static bool pressed = false;
     XPT2046_update();
+    XPT2046_TouchData_t touch = XPT2046_getTouch();
+    if(touch.pressure) {
+        if(!pressed) {
+            pressed = true;
+            gpio_xor_mask(1 << DEBUG2_PIN);
+        }
+    }
+    else {
+        if(pressed) {
+            pressed = false;
+            gpio_xor_mask(1 << DEBUG2_PIN);
+        }
+    }
 }
 
 /**
@@ -152,4 +173,16 @@ void initHardware() {
     // init hardware
     ili9488_init();
     XPT2046_Init();
+}
+
+/**
+ * @brief Handler for touch events
+ * 
+ */
+void touchHandler(uint16_t x, uint16_t y) {
+    XPT2046_update();
+    XPT2046_TouchData_t touch = XPT2046_getTouch();
+    if(touch.pressure) {
+        gpio_xor_mask(1 << DEBUG2_PIN);
+    }
 }
